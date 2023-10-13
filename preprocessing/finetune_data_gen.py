6f9abd7b27981c7dfd0e5853ff92a5b47d56f802
matths2 <mattias.holmgren@tietoevry.com>
2023-10-13 13:13:19 +0200

    rm submodule. todo create fork submodule instead

--- preprocessing/finetune_data_gen.py
+++ preprocessing/finetune_data_gen.py
@@ -1,6 +1,6 @@
 import json
-import csv
 import os
+import random
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 input_file = os.path.join(
@@ -14,6 +14,13 @@ with open(input_file, "r") as json_file:
     data = json.load(json_file)
 
 prompts = []
+alt_intro_phrases = [
+    "I'm glad you asked, let me explain.",
+    "That's an excellent question, let me guide you through it.",
+    "Fantastic question! Allow me to clarify.",
+    "Great question! Let me break it down for you.",
+    "I'm excited you asked! Here's the explanation.",
+]
 
 # Iterate through the JSON data
 for item in data:
@@ -21,60 +28,83 @@ for item in data:
     mpn = part["mpn"]
     name = part["name"]
     shortDescription = part["shortDescription"]
-    # handle lists
-    specs = part["specs"]
-    spec_info = map(
-        lambda spec: (spec["attribute"]["name"], spec["value"], spec["units"]), specs
-    )
-    datasheet = part["bestDatasheet"]
-    datasheet_req_keys = ["name", "pageCount", "url", "creditString", "creditUrl"]
-    document_collections = part["documentCollections"]
-    document_info = list(
-        map(
-            lambda collection: list(
-                map(
-                    lambda document: f"Collection Name: {collection['name']}\n   * Document Name: {document.get('name', 'N/A')}\n   * Page Count: {document.get('pageCount', 'N/A')}\n   * URL: {document.get('url', 'N/A')}\n   * Credit String: {document.get('creditString', 'N/A')}\n   * Credit URL: {document.get('creditUrl', 'N/A')}",
-                    collection["documents"],
-                )
-            ),
-            document_collections,
-        )
-    )
-    document_info = [item for sublist in document_info for item in sublist]
-    formatted_document_info = "\n   * " + "\n   * ".join(document_info)
+    estimatedFactoryLeadDays = part["estimatedFactoryLeadDays"]
+    category = part["category"]
+    if category:
+        category_blurb = part["category"]["blurb"]
 
-    # Now 'formatted_document_info' contains the formatted document information
+    # handle lists
+    # Be advised to check that they're not empty, they may be at this stage.
+    specs = part["specs"]
+    if specs:
+        spec_info = map(
+            lambda spec: (spec["attribute"]["name"], spec["value"], spec["units"]),
+            specs,
+        )
+    similarParts = part["similarParts"]
+    if similarParts:
+        similarParts_info = map(lambda part: (part["mpn"]), similarParts)
 
     # Create the input prompts and outputs based on the extracted information
-    # Prompt 1: Specs
-    input_spec_prompt = f"What are the specs for mpn {mpn}?"
-    output_spec_prompt = (
-        f"{name} description: {shortDescription}\n"
-        "Here are some additional specs:\n"
-        "   * "
-        + "\n   * ".join(
-            [f"{name}: {value} {units}" for name, value, units in spec_info]
-        )
-    )
 
-    # Prompt 2: Documentation
-    input_datasheet_prompt = f"What documentation is available for mpn {mpn}?"
-    output_datasheet_prompt = f"\nDatasheet available for {name}:\n"
-    if datasheet is not None and isinstance(datasheet, dict):
-        output_datasheet_prompt += "\n".join(
-            [f"   * {key}: {datasheet.get(key, 'N/A')}" for key in datasheet_req_keys]
+    # Prompt 1: Specs
+    if specs and spec_info:
+        input_spec_prompt = f"What are the specs for mpn {mpn}?"
+        output_spec_prompt = (
+            f"\n{name} description: {shortDescription}\n"
+            "Here are some additional specs:\n"
+            "   * "
+            + "\n   * ".join(
+                [f"{name}: {value} {units}" for name, value, units in spec_info]
+            )
         )
-    output_datasheet_prompt += (
-        "\n\nAdditional documents:" + "\n   * " + "\n   * ".join(document_info)
-    )
+        prompts.append(
+            {"instruction": input_spec_prompt, "response": output_spec_prompt}
+        )
+
+    # Prompt 2: similarParts
+    if similarParts and similarParts_info:
+        input_similarParts_prompt = (
+            f"Can you suggest some parts that are similar to {mpn}?"
+        )
+        output_similarParts_prompt = (
+            f"\n Sure thing, here is a list of parts that are similar to {name}\n"
+            "   * " + "\n   * ".join([f"{mpn}" for mpn in similarParts_info])
+        )
+        prompts.append(
+            {
+                "instruction": input_similarParts_prompt,
+                "response": output_similarParts_prompt,
+            }
+        )
+
+    # Prompt 3: blurb
+    intro_phrase = random.choice(alt_intro_phrases)
+    if category and category_blurb:
+        input_blurb_prompt = f"What are {category_blurb['name']}?"
+        output_blurb_prompt = (
+            f"{intro_phrase}\n{category_blurb['description']}.\n"
+            f"And {category_blurb['content']}"
+        )
+        prompts.append(
+            {"instruction": input_blurb_prompt, "response": output_blurb_prompt}
+        )
+
+    # Prompt 4: estimatedFactoryLeadDays
+    if estimatedFactoryLeadDays:
+        input_estimatedFactoryLeadDays_prompt = (
+            f"What is the estimated factory lead time in days for {mpn}?"
+        )
+        output_estimatedFactoryLeadDays_prompt = f"The estimated factory lead time for {name} is {estimatedFactoryLeadDays} days"
+        prompts.append(
+            {
+                "instruction": input_estimatedFactoryLeadDays_prompt,
+                "response": output_estimatedFactoryLeadDays_prompt,
+            }
+        )
 
     # Prompt ...
     # ...
-
-    prompts.append({"instruction": input_spec_prompt, "response": output_spec_prompt})
-    prompts.append(
-        {"instruction": input_datasheet_prompt, "response": output_datasheet_prompt}
-    )
 
 # Iterate through the prompts and write them to the JSONL file
 with open(output_file, mode="w") as jsonl_file:
--- preprocessing/standardizer_delux.py
+++ preprocessing/standardizer_delux.py
@@ -23,68 +23,57 @@ for filename in os.listdir(data_directory):
                 if isinstance(datasheets, list):
                     for datasheet in datasheets:
                         part = datasheet.get("part", {})
-                        lists_not_empty = all(
-                            value
-                            for value in [
-                                part.get("specs", []),
-                                part.get("descriptions", []),
-                                part.get("documentCollections", []),
-                            ]
+
+                        id = part.get("id", "")
+                        slug = part.get("slug", "")
+                        mpn = part.get("mpn", "")
+                        genericMpn = part.get("genericMpn", "")
+                        name = part.get("name", "")
+                        shortDescription = part.get("shortDescription", "")
+                        octopartUrl = part.get("octopartUrl", "")
+                        v3uid = part.get("v3uid", "")
+                        counts = part.get("counts", {})
+                        totalAvail = part.get("totalAvail", 0)
+                        avgAvail = part.get("avgAvail", 0)
+                        manufacturer = part.get("manufacturer", {})
+                        specs = part.get("specs", [])
+                        descriptions = part.get("descriptions", [])
+                        estimatedFactoryLeadDays = part.get(
+                            "estimatedFactoryLeadDays", 0
                         )
+                        similarParts = part.get("similarParts", [])
+                        category = part.get("category", {})
+                        bestDatasheet = part.get("bestDatasheet", {})
+                        documentCollections = part.get("documentCollections", [])
+                        medianPrice1000 = part.get("medianPrice1000", {})
 
-                        if lists_not_empty:
-                            id = part.get("id", "")
-                            slug = part.get("slug", "")
-                            mpn = part.get("mpn", "")
-                            genericMpn = part.get("genericMpn", "")
-                            name = part.get("name", "")
-                            shortDescription = part.get("shortDescription", "")
-                            octopartUrl = part.get("octopartUrl", "")
-                            v3uid = part.get("v3uid", "")
-                            counts = part.get("counts", {})
-                            totalAvail = part.get("totalAvail", 0)
-                            avgAvail = part.get("avgAvail", 0)
-                            manufacturer = part.get("manufacturer", {})
-                            specs = part.get("specs", [])
-                            descriptions = part.get("descriptions", [])
-                            estimatedFactoryLeadDays = part.get(
-                                "estimatedFactoryLeadDays", 0
-                            )
-                            category = part.get("category", {})
-                            bestDatasheet = part.get("bestDatasheet", {})
-                            documentCollections = part.get("documentCollections", [])
-                            medianPrice1000 = part.get("medianPrice1000", {})
-
-                            # Add the extracted data to the standardized_data list
-                            standardized_data.append(
-                                {
-                                    "part": {
-                                        "id": id,
-                                        "slug": slug,
-                                        "mpn": mpn,
-                                        "genericMpn": genericMpn,
-                                        "name": name,
-                                        "shortDescription": shortDescription,
-                                        "octopartUrl": octopartUrl,
-                                        "v3uid": v3uid,
-                                        "counts": counts,
-                                        "totalAvail": totalAvail,
-                                        "avgAvail": avgAvail,
-                                        "manufacturer": manufacturer,
-                                        "specs": specs,
-                                        "descriptions": descriptions,
-                                        "estimatedFactoryLeadDays": estimatedFactoryLeadDays,
-                                        "category": category,
-                                        "bestDatasheet": bestDatasheet,
-                                        "documentCollections": documentCollections,
-                                        "medianPrice1000": medianPrice1000,
-                                    }
+                        # Add the extracted data to the standardized_data list
+                        standardized_data.append(
+                            {
+                                "part": {
+                                    "id": id,
+                                    "slug": slug,
+                                    "mpn": mpn,
+                                    "genericMpn": genericMpn,
+                                    "name": name,
+                                    "shortDescription": shortDescription,
+                                    "octopartUrl": octopartUrl,
+                                    "v3uid": v3uid,
+                                    "counts": counts,
+                                    "totalAvail": totalAvail,
+                                    "avgAvail": avgAvail,
+                                    "manufacturer": manufacturer,
+                                    "specs": specs,
+                                    "similarParts": similarParts,
+                                    "descriptions": descriptions,
+                                    "estimatedFactoryLeadDays": estimatedFactoryLeadDays,
+                                    "category": category,
+                                    "bestDatasheet": bestDatasheet,
+                                    "documentCollections": documentCollections,
+                                    "medianPrice1000": medianPrice1000,
                                 }
-                            )
-                        else:
-                            print(f"Skipping {filename}: JSON data is missing data")
-                            excluded += 1
-
+                            }
+                        )
                 else:
                     print(
                         f"Skipping {filename}: JSON data is not in the expected list format"
@@ -101,7 +90,9 @@ print(f"Excluded {excluded} files due to errors.")
 
 # Write the standardized_data list to a JSON file in the script's directory
 output_file_name = "standardized_data_delux.json"
-output_file_path = os.path.join(script_dir, "..", "datasets", output_file_name)
+output_file_path = os.path.join(
+    script_dir, "..", "datasets", "sensitive_data", output_file_name
+)
 with open(output_file_path, "w") as output_file:
     json.dump(standardized_data, output_file, indent=2)
 
